7766fa233d35eb9aec2e45fdf8cd5f0f86bc3e49
ClPhillips10 <Lakephillips3232@gmail.com>
2023-05-07 10:17:40 -0500

    added variables

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -1,10 +1,41 @@
 ﻿namespace VariablesExercise
 {
-    public class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            string dogname = "Ralph";
+            int dogAge = 6;
+            char initial = 'R';
+            bool isCat = false;
+            double weight = 25.23;
+
+
+
+
+
+
+            Console.WriteLine("my dogs name is " + dogname + ", he is " + dogAge);
+            Console.Write("Ralph is a cat. T or F? Answer :");
+
+
+
+
+            Console.WriteLine(isCat);
+            Console.Write("what is ralphs initial? ");
+            Console.WriteLine(initial);
+
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
